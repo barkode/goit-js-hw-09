5bf65e4ddefa4f5db1150cd528bdf93c09a5433a
barkode <konstantinks@gmail.com>
2023-04-11 00:28:58 +0100

    first end

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+import Notiflix from 'notiflix';
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+stopBtn.setAttribute('disabled', 'disabled');
+
+startBtn.addEventListener('click', () => {
+  startBtn.setAttribute('disabled', 'disabled');
+  stopBtn.removeAttribute('disabled');
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.removeAttribute('disabled');
+  clearInterval(intervalId);
+  stopBtn.setAttribute('disabled', 'disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
