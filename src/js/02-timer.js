7424148c4aa8560afbea7c67b0f7e2c3338023ae
barkode <konstantinks@gmail.com>
2023-04-11 18:48:08 +0100

    add stop button

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,14 +2,53 @@ import { Notify } from 'notiflix';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const btnStart = document.querySelector('[data-start]');
+//const btnStop = document.querySelector('[data-stop]');
+const fieldDays = document.querySelector('[data-days]');
+const fieldHours = document.querySelector('[data-hours]');
+const fieldMinutes = document.querySelector('[data-minutes]');
+const fieldSeconds = document.querySelector('[data-seconds]');
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0] < new Date()) {
+      Notify.failure('Please choose a date in the future');
+      btnStart.disabled = true;
+      return;
+    }
+    btnStart.disabled = false;
   },
 };
 
-flatpickr('#datetime-picker', options);
+//btnStop.disabled = true;
+btnStart.disabled = true;
+
+const fp = flatpickr('#datetime-picker', options);
+console.log(fp.selectedDates[0]);
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
