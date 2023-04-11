e12b7e2a05376dccca28d59d9e5a53d3e61c1338
barkode <konstantinks@gmail.com>
2023-04-11 18:01:28 +0100

    add some inits

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,15 @@
 import { Notify } from 'notiflix';
 import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr('#datetime-picker', options);
